f00ec478f8b60aa32d0d8a2908d50c00df2f1bbf
Ana Rosiles <arosiles@hawk.iit.edu>
2023-10-05 17:25:27 -0500

    trying jquery email validation

--- js/site.js
+++ js/site.js
@@ -3,3 +3,31 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+var email_val = "";
+var regexSimpleEmail = /@/;
+
+$("#submitBtn").on("click",function(){
+    email_val = $("input").val();
+    if (validateEmail(email_val,regexSimpleEmail)){
+        $("#result").addClass("isemail").show().html(`Your email ${email_val} is valid!`);
+    }
+    else{
+        $("#result").removeClass("isemail").show().html(`Sorry, Your email ${email_val} is invalid!`);
+    }
+});
+
+
+function validateEmail(email_val,emailRegex){
+    if (emailRegex.test(email_val)){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+$("#clear").on("click",function(){
+    $("#result").hide().html('')
+    $("#inputemail").val("");
+})
